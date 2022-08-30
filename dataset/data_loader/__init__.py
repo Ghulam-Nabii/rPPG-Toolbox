f30c65a8375fec1ac939e44b3446da6d239e8f48
Girish Narayanswamy <girishvn@klone1.hyak.uw.edu>
2022-08-29 22:38:40 -0700

    new branch for multi-dataset training

--- dataset/data_loader/__init__.py
+++ dataset/data_loader/__init__.py
@@ -2,5 +2,7 @@ import dataset.data_loader.BaseLoader
 import dataset.data_loader.COHFACELoader
 import dataset.data_loader.UBFCLoader
 import dataset.data_loader.PURELoader
+import dataset.data_loader.AFRLLoader
+import dataset.data_loader.MultiDatasetLoader
 import dataset.data_loader.SyntheticsLoader
 
